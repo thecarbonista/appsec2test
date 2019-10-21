9dfb6bb78800f56a63f5c7e39e3a360bf83136af
thecarbonista <thecarbonista@gmail.com>
2019-10-21 14:50:18 -0700

    moved id tags to forms.py
    Signed-off-by: thecarbonista <thecarbonista@gmail.com>

--- forms.py
+++ forms.py
@@ -17,7 +17,9 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError(id='success', 'failure')
+        else:
+            return StringField(id='success', 'success')
 
 
 class LoginForm(FlaskForm):
